73db568d5a40d9fee033644f3ca459b766a63eb2
RoxBibop <thibault.malik@gmail.com>
2020-04-02 10:41:29 +0200

    modif font

--- src/components/App.js
+++ src/components/App.js
@@ -6,35 +6,35 @@ import Form from './form.jsx'
 import '../style/App.css';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { isShow : "login" };
-	}
+  constructor(props) {
+    super(props);
+    this.state = { isShow : "login" };
+  }
 
-	toggleTeacher = () => {
+  toggleTeacher = () => {
     this.setState(state => ({ isShow: "teach"}));
-	}
+  }
 
-	toggleSign = () => {
-		this.setState(state => ({ isShow: "sign"}));
-	}
+  toggleSign = () => {
+    this.setState(state => ({ isShow: "sign"}));
+  }
 
-	render() {
-		return (
-			<div className="app">
-				{(() => {
-					switch(this.state.isShow){
+  render() {
+    return (
+      <div className="app">
+        {(() => {
+          switch(this.state.isShow){
             case 'login':
               return <Login teacher={this.toggleTeacher} sign={this.toggleSign}/>;
             case 'teach' :
               return <Form/>;
             case 'sign' : 
               return <Sign/>;
-					}
-				})()}
-			</div>
-		);
-	}
+          }
+        })()}
+      </div>
+    );
+  }
 }
 
 export default App;
