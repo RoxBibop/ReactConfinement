eb92b62be140a8bb846648a2d1eb580882f5f1ca
RoxBibop <thibault.malik@gmail.com>
2020-05-26 14:35:12 +0200

    duplaction d'inputs

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -7,50 +7,31 @@ class AddUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      delUsers :this.delUsers(),
-      addUsers : this.addUsers()
+      inputs : [],
     }
-    this.addUsers = this.addUsers.bind(this);
-  }
-  
-    addUsers = () => {
-      // Add user with button "+"/ duplicate input : name/lastname/mail
-    console.log("coucou");
-    let i = 0;
-    const formAdduser = document.querySelector("#inputRow");
-    const clone = formAdduser.cloneNode(true);
-    const cloneId = formAdduser + ++i;
-    cloneId.parentNode.appendChild(clone);
-    
-    return 
   }
 
-    delUsers = () => {
-      // delete user 
-      console.log("delete");
-      return
-    }
+  addUser () {
+    this.setState({inputs: [...this.state.inputs, ""]})
+  }
 
-    infoRegister = () => {
-      console.log("bouh");
-    }
+
 
   
-  render() { 
+  render () { 
     return ( 
       <div className ="stepperTwo">
-          <form>
-            <h2>Ajoutez des apprenants</h2>
-            <div id="inputRow">
-              <input id="name" type="name" value="Nom"/>
-              <input id="lastname" type="lastname" value="Prénom"/>
-              <input id="email" type="mail" name="email" value="e-mail"/>
-              <div className= "button del" onClick={this.delUsers}>-</div>
-            </div>
-            <div>
-              <div className="button add" onClick={this.addUsers}>+</div>
-            </div>
-          </form>
+        {
+          this.state.inputs.map((inp, index)=>{
+            return (
+              <div key={index}>
+                <input></input>
+                <input></input> 
+              </div>
+            )
+          })
+        }
+        <p onClick={(e) => this.addUser(e)}>AJOUTER</p>
       </div>
     );
   }
