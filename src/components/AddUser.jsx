e71e70bcb198c3546f5d8ec9377d09e1211d1792
RoxBibop <thibault.malik@gmail.com>
2020-05-27 10:41:27 +0200

    gestion multiple inputs

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -9,12 +9,14 @@ class AddUser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputs : [],
+      inputs : [{ index: Math.random(), userName: "", userLastName: "", userMail: ""}],
     }
   }
 
   addUser () {
-    this.setState({inputs: [...this.state.inputs, ""]})
+    this.setState((prevState) => ({
+      inputs: [...prevState.inputs, {index: Math.random(), userName: "", userLastName: "", userMail: ""}]
+    }));
   }
 
   change(e, index) {
@@ -34,21 +36,25 @@ class AddUser extends React.Component {
       <div className ="stepperTwo">
         {
           this.state.inputs.map((inp, index)=>{
+            let userName = `username${index}`, 
+                userLastName = `userlast${index}`,
+                userMail = `usermail${index}`;
+
             return (
               <div className="inputs" key={index}>
                 <span className="num">{index + 1}</span>
                 <div className="inputContainer">
                   <div>
                     <label>Nom :</label>
-                    <input onChange={(e) => this.change(e, index)} id="name" type="name" value={inp}/>
+                    <input type="text" name="username" data-id={index} id={userName}/>
                   </div>
                   <div>
                     <label>Prénom :</label>
-                    <input onChange={(e) => this.change(e, index)} id="lastname" type="lastname" value={inp}/>
+                    <input type="text" name="userlast" data-id={index} id={userLastName}/>
                   </div>
                   <div>
                     <label>Mail :</label>
-                    <input onChange={(e) => this.change(e, index)} id="email" type="mail" name="email" value={inp}/>
+                    <input type="text" name="usermail" data-id={index} id={userMail}/>
                   </div>
                 </div>
                 <p className="trash">
